aaa57dbbfba3210c350b82f300b84e1d68aaafd3
WammKD <jaft.r@outlook.com>
2018-05-13 15:45:43 -0500

    Add another constant and try using the new constants in my test code.

--- github.scm
+++ github.scm
@@ -2,12 +2,14 @@
 
 (define     URL_BASE         "https://api.github.com")
 (define CONTENT_TYPE "application/vnd.github.v3+json")
+(define   USER_AGENT              "Guile Octokit SDK")
 
-
+(define (github~convert-api-call callBody)
+  (json-string->scm (utf8->string callBody)))
 
 (receive (header body)
     (http-get
-      "https://api.github.com/users/WammKD"
-      #:headers '((User-Agent . "Awesome-Octocat-App")
-                  (Accept     . "application/vnd.github.v3+json")))
-  (hash-ref (json-string->scm (utf8->string body)) "avatar_url"))
+      (string-append URL_BASE "/users/WammKD")
+      #:headers `((User-Agent .   ,USER_AGENT)
+                  (Accept     . ,CONTENT_TYPE)))
+  (hash-ref (github~convert-api-call body) "avatar_url"))
